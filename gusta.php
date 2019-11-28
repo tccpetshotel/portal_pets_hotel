b91659b9f304469d36f3a0a041abb287816e14c8
unknown <30698@fiecalunos.lan>
2019-11-27 22:49:56 -0300

    Edit in HeaderFooter

--- gusta.php
+++
@@ -1,3 +0,0 @@
-<?php include_once ("./headEFooter/head.php");?>
-<?php include_once ("content.php"); ?>
-<?php include_once ("./headEFooter/footer.php");?>
--- headEFooter/head.php
+++ headEFooter/head.php
@@ -12,6 +12,7 @@
 </head>
 <body>
 <nav>
+  <div class="col s12 m8 l3">
     <div class="nav-wrapper">
       <a href="#" class="brand-logo">Logo</a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
@@ -20,6 +21,7 @@
         <li><a href="collapsible.html">JavaScript</a></li>
       </ul>
     </div>
-  </nav>
+  </div>
+</nav>
 </body>
 </html>
