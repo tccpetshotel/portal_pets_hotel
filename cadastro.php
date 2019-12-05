a3da522b55ee972a11a888e595e3c23bceaadda5
unknown <30687@fiecalunos.lan>
2019-12-04 22:52:33 -0300

    Add Style on Sigin

--- cadastro.php
+++ cadastro.php
@@ -5,11 +5,11 @@
 <div id="cadastro" class="container row">
     <form class="col s12 m12" action="https://petshotel.herokuapp.com/client/create" method="POST">
         <div class="row">
-            <div class="input-field col s6 m6">
+            <div class="input-field col s12 m6">
                 <input id="nome" name="nome" type="text" class="validate">
                 <label for="nome">Nome</label>
             </div>
-            <div class="input-field col s6 m6">
+            <div class="input-field col s12 m6">
                 <input id="email" name="email" type="email" class="validate">
                 <label for="email">Email</label>
             </div>
@@ -17,19 +17,19 @@
                 <input id="senha" name="senha" type="password" class="validate">
                 <label for="senha">Senha</label>
             </div>
-            <div class="input-field col s6 m6">
+            <div class="input-field col s12 m6">
                 <input id="endereco" name="endereco" type="text" class="validate">
                 <label for="endereco">Endereço</label>
             </div>
-            <div class="input-field col s6 m6">
+            <div class="input-field col s12 m6">
                 <input id="cidade" name="cidade" type="text" class="validate">
                 <label for="cidade">Cidade</label>
             </div>
-            <div class="input-field col s6 m6">
+            <div class="input-field col s12 m6">
                 <input id="estado" name="estado" type="text" class="validate">
                 <label for="estado">Estado</label>
             </div>
-            <div class="input-field col s6 m6">
+            <div class="input-field col s12 m6">
                 <input id="celular" name="celular" type="tel" class="validate">
                 <label for="celular">Celular</label>
             </div>
