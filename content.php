5c167c6b5af8264eb7fbccd51e89420e513005b7
unknown <30687@fiecalunos.lan>
2019-12-05 21:06:57 -0300

    Add release

--- content.php
+++ content.php
@@ -1,47 +1,47 @@
   <div id="portfolio" class="cx gray">
-      <div class="db">
-        <div class="b e" style="position: relative; height: 3482px;">
-          <div class="d hx hf gu gallery-item ce all" style="position: absolute; left: 0px; top: 0px;">
-            <div class="h blue-grey darken-1">
-            </div>
+    <div class="db">
+      <div class="b e" style="position: relative; height: 3482px;">
+        <div class="d hx hf gu gallery-item ce all" style="position: absolute; left: 0px; top: 0px;">
+          <div class="h blue-grey darken-1">
           </div>
+        </div>
 
-          <div 
-          class="d hx hf gu gallery-item gallery-expand ce overhead" 
-          style="position: absolute; left: 0px; top: 226px;" 
-          data-fillscreen="true"
-          >
+        <div class="d hx hf gu gallery-item gallery-expand ce overhead" style="position: absolute; left: 0px; top: 226px;" data-fillscreen="true">
           <div class="placeholder">
             <div class="gallery-curve-wrapper">
               <a class="gallery-cover gray" style="height: 200px;">
                 <img class="responsive-img" src="http://www.petz.com.br/blog/wp-content/uploads/2019/11/passaros-brasileiros-tucano-toco.jpg" alt="placeholder">
-              <div class="gradient"></div></a>
+                <div class="gradient"></div>
+              </a>
               <div class="gallery-header">
-                  <!--Content WorkSpace-->
+                <!--Content WorkSpace-->
                 <span style="font-family:cursive; font-weight: bold;"> Pássaros brasileiros: conheça as 5 espécies mais famosas!</span>
               </div>
               <div class="gallery-body">
                 <div class="title-wrapper">
-                    <h3>Pássaros brasileiros</h3>
+                  <h3>Pássaros brasileiros</h3>
                 </div>
                 <p class="fi">
-                  Dos belos e charmosos tucanos aos pequenos e afinados sabiás, os pássaros brasileiros fazem muito sucesso. Com penas coloridas e belas tonalidades, não tem como não se apaixonar por esses animais encantadores!                
+                  Dos belos e charmosos tucanos aos pequenos e afinados sabiás, os pássaros brasileiros fazem muito sucesso. Com penas coloridas e belas tonalidades, não tem como não se apaixonar por esses animais encantadores!
                 </p>
                 <h3>O que são espécies nativas de pássaros brasileiros?</h3>
-                <p class="fi">    
+                <p class="fi">
                   Antes de falarmos desses animais, é importante entender o que é uma espécie nativa. O IBAMA, hoje, divide as aves em três categorias: domésticos, exóticos e pássaros silvestres brasileiros.
                 </p>
-                </div>
+              </div>
             </div>
-          </div></div>
-          <div class="d hx hf gu gallery-item gallery-expand ce overhead" style="position: absolute; left: 0px; top: 608px;" data-fillscreen="true"><div class="placeholder">
+          </div>
+        </div>
+        <div class="d hx hf gu gallery-item gallery-expand ce overhead" style="position: absolute; left: 0px; top: 608px;" data-fillscreen="true">
+          <div class="placeholder">
             <div class="gallery-curve-wrapper">
               <a class="gallery-cover gray" style="height: 200px;">
                 <img class="responsive-img" src="http://www.petz.com.br/blog/wp-content/uploads/2019/11/tipos-de-hamster-roedor-s%C3%ADrio.jpg" alt="placeholder">
-              <div class="gradient"></div></a>
+                <div class="gradient"></div>
+              </a>
               <div class="gallery-header">
 
-              <!-- Content Studio-->
+                <!-- Content Studio-->
                 <span style="font-family:cursive; font-weight: bold;">Tipos de hamster: escolha o ideal para seu lar!</span>
               </div>
               <div class="gallery-body">
@@ -53,57 +53,57 @@
                 </p>
                 <h3>Diferentes tipos de hamster</h3>
                 <p class="fi">
-                  De acordo com veterinários, há cerca de 24 espécies, normalmente chamadas de hamsters. Entre todos os tipos de hamster, porém, apenas algumas são criadas como pets. 
-                </p>  
-            </div>
+                  De acordo com veterinários, há cerca de 24 espécies, normalmente chamadas de hamsters. Entre todos os tipos de hamster, porém, apenas algumas são criadas como pets.
+                </p>
+              </div>
             </div>
           </div>
-          </div>
-          <div class="d hx hf gu gallery-item gallery-expand ce sintel" style="position: absolute; left: 0px; top: 1090px;" data-fillscreen="true"><div class="placeholder">
+        </div>
+        <div class="d hx hf gu gallery-item gallery-expand ce sintel" style="position: absolute; left: 0px; top: 1090px;" data-fillscreen="true">
+          <div class="placeholder">
             <div class="gallery-curve-wrapper">
               <a class="gallery-cover gray" style="height: 200px;">
                 <img class="responsive-img" src="http://www.animalepetshop.com.br/wp-content/uploads/2019/02/coelho-comendo.jpg" alt="placeholder">
-              <div class="gradient"></div></a>
+                <div class="gradient"></div>
+              </a>
               <div class="gallery-header">
-              <!--Content Bamboo-->
-              <span style="font-family:cursive; font-weight: bold;">Como oferecer comida para coelhos?</span>
-            </div>
-            <div class="gallery-body">
-              <div class="title-wrapper">
-                <p class="fi">   
-                Quando pensamos em alimentos para coelhos, muitas vezes já imaginamos verduras e muitas cenouras, não é mesmo? Mas, será que essa é a melhor forma de alimentar coelho? Especialistas indicam que, na verdade, a nutrição desses fofíssimos pets é mais delicada do que se pode imaginar.
-                </p>      
+                <!--Content Bamboo-->
+                <span style="font-family:cursive; font-weight: bold;">Como oferecer comida para coelhos?</span>
+              </div>
+              <div class="gallery-body">
+                <div class="title-wrapper">
+                  <p class="fi">
+                    Quando pensamos em alimentos para coelhos, muitas vezes já imaginamos verduras e muitas cenouras, não é mesmo? Mas, será que essa é a melhor forma de alimentar coelho? Especialistas indicam que, na verdade, a nutrição desses fofíssimos pets é mais delicada do que se pode imaginar.
+                  </p>
                   <h3>O que é permitido para alimentar coelhos?</h3>
                 </div>
                 <p class="fi">
                   Portanto, agora já sabemos que é preciso estar atento aos alimentos para coelhos. Mas será que cenouras e verduras estão proibidas? Afinal, o que o coelho pode comer? De acordo com veterinários de pets exóticos, coelhos podem, sim, comer vegetais.
                 </p>
-            </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
 
-        </div>
+    </div>
 
-      </div>
+  </div>
 
-    </div><!-- /.container -->
+  </div><!-- /.container -->
 
-    <!-- Core Javascript -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/materialize/0.98.0/js/materialize.min.js"></script>
-    <script src="//cdn.shopify.com/s/files/1/1775/8583/t/1/assets/gallery.min.opt.js?0"></script>
+  <!-- Core Javascript -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/materialize/0.98.0/js/materialize.min.js"></script>
+  <script src="//cdn.shopify.com/s/files/1/1775/8583/t/1/assets/gallery.min.opt.js?0"></script>
 
-    <!--Buttons-->
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
-    <script type='text/javascript' src='https://platform-api.sharethis.com/js/sharethis.js#property=590ca7fcef4e140012286a59&product=sticky-share-buttons' async='async'></script>
-<div class="hiddendiv common">
+  <!--Buttons-->
+  <script async defer src="https://buttons.github.io/buttons.js"></script>
+  <script type='text/javascript' src='https://platform-api.sharethis.com/js/sharethis.js#property=590ca7fcef4e140012286a59&product=sticky-share-buttons' async='async'></script>
+  <div class="hiddendiv common">
 
-</div>
-<div class="drag-target" 
-data-sidenav="nav-mobile" 
-style="left: 0px; touch-action: pan-y; -webkit-user-drag: none; 
+  </div>
+  <div class="drag-target" data-sidenav="nav-mobile" style="left: 0px; touch-action: pan-y; -webkit-user-drag: none; 
 -webkit-tap-highlight-color: rgba(0, 0, 0, 0);">
-</div>
-</body>
+  </div>
+  </body>
